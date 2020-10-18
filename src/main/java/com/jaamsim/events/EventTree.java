8a0ccc960a51c56c6ceed0bc5bdcb4aa885b0685
zhanyeye <zhanyeye@qq.com>
2020-10-18 20:38:41 +0800

    finish read EventTree

--- src/main/java/com/jaamsim/events/EventTree.java
+++ src/main/java/com/jaamsim/events/EventTree.java
@@ -36,26 +36,46 @@ class EventTree {
 	 */
 	private EventNode lowest = null;
 
-	///////////////////////////////////////////
-	// Scratch space, used instead of having parent pointers
 
+
+	/**
+	 * Scratch space, used instead of having parent pointers
+	 * 暂存空间，用来代替父指针
+	 */
 	private EventNode[] scratch = new EventNode[64];
 	private int scratchPos = 0;
 
+	/**
+	 * 将节点放到暂存空间中
+	 * @param n
+	 */
 	private void pushScratch(EventNode n) {
 		scratch[scratchPos++] = n;
 	}
 
+	/**
+	 * 从后向前删除n个暂存节点
+	 * @param n
+	 */
 	private void dropScratch(int n) {
 		scratchPos = Math.max(0, scratchPos - n);
 	}
 
-	// Get the 'nth' node from the end of the scratch (1 being the first)
+
+	/**
+	 * Get the 'nth' node from the end of the scratch (1 being the first)
+	 * 从scratch中，取出倒数第n个节点
+	 * @param n 倒数第几个节点
+	 * @return
+	 */
 	private EventNode getScratch(int n) {
 		return (scratchPos >= n) ? scratch[scratchPos - n] : null;
 	}
 
 
+	/**
+	 * 重置暂存空间（将它的索引置0）
+	 */
 	private void resetScratch() {
 		scratchPos = 0;
 	}
@@ -100,7 +120,7 @@ class EventTree {
 	}
 
 	/**
-	 * 根据调度刻度和优先级去查询或创建节点
+	 * 根据调度刻度和优先级去查询节点，若没有则创建一个
 	 * @param schedTick
 	 * @param priority
 	 * @return
@@ -166,16 +186,29 @@ class EventTree {
 
 		// 获取该节点的父节点
 		EventNode parent = getScratch(1);
-		if (parent == null || !parent.red) return; // cases 1 and 2
+		if (parent == null || !parent.red) {
+			// cases 1 and 2: 没有父节点和父节点为黑色的情况
+			return;
+		}
 
 		// 获取祖父节点
 		EventNode gp = getScratch(2);
-		if (gp == null) return;
+		if (gp == null) {
+			// 没有祖父节点，父节点为根节点的情况（即父节点为黑色）
+			return;
+		}
 
+		// 获取叔叔节点
 		EventNode uncle = (gp.left == parent ? gp.right : gp.left);
-		if (uncle.red) {
+
+		  if (uncle.red) {
 			// Both parent and uncle are red
-			// case 2
+			// case 3：
+			// 1. 新增当前节点，默认颜色为红色;
+			// 2. 父节点变成黑色
+			// 3. 叔叔节点U变成黑色
+			// 4. 祖父节点变成红色
+			// 5. 如果祖父节点变红不满足红黑树性质，则将祖父节点当成新增节点递归处理
 			parent.red = false;
 			uncle.red = false;
 			gp.red = true;
@@ -185,6 +218,7 @@ class EventTree {
 		}
 
 		// case 4
+
 		if (n == parent.right && gp != null && parent == gp.left) {
 			// Right child of a left parent, rotate left at parent
 			parent.rotateLeft(gp);
@@ -243,6 +277,7 @@ class EventTree {
 		}
 
 		// Debugging
+		// 如果节点所指向的链表仍然有元素，则抛出异常
 		if (current.head != null || current.tail != null)
 			throw new RuntimeException("Removing non-empy node");
 
@@ -297,6 +332,11 @@ class EventTree {
 		return true;
 	}
 
+	/**
+	 * 将该节点和叶子节点进行交换，删除节点时可能调用
+	 * @param node
+	 * @return
+	 */
 	private EventNode swapToLeaf(EventNode node) {
 		pushScratch(node);
 		EventNode curr = node.left;
@@ -308,6 +348,10 @@ class EventTree {
 		return curr;
 	}
 
+	/**
+	 * 删除节点后的平衡操作
+	 * @param n
+	 */
 	private void deleteBalance(EventNode n) {
 		// At all times the scratch space should contain the parent list (but not n)
 		EventNode parent = getScratch(1);
@@ -389,13 +433,23 @@ class EventTree {
 		}
 	}
 
+	/**
+	 * 运行整棵树的所有runner
+	 * @param runner
+	 */
 	final void runOnAllNodes(EventNode.Runner runner) {
 		runOnNode(root, runner);
 	}
 
+	/**
+	 * 运行某个节点所有子树的所有runner
+	 * @param node
+	 * @param runner
+	 */
 	private void runOnNode(EventNode node, EventNode.Runner runner) {
-		if (node == EventNode.nilNode)
+		if (node == EventNode.nilNode) {
 			return;
+		}
 
 		runOnNode(node.left, runner);
 
@@ -404,81 +458,7 @@ class EventTree {
 		runOnNode(node.right, runner);
 	}
 
-	// Verify the sorting structure and return the number of nodes
-	final int verify() {
-		if (root == EventNode.nilNode) return 0;
 
-		if (EventNode.nilNode.red == true)
-			throw new RuntimeException("nil node corrupted, turned red");
-		return verifyNode(root);
-	}
-
-	private int verifyNode(EventNode n) {
-		int lBlacks = 0;
-		int rBlacks = 0;
-
-		if (n.left != EventNode.nilNode) {
-			if (n.compareToNode(n.left) != 1)
-				throw new RuntimeException("RB tree order verify failed");
-			lBlacks = verifyNode(n.left);
-		}
-		if (n.right != EventNode.nilNode) {
-			if (n.compareToNode(n.right) != -1)
-				throw new RuntimeException("RB tree order verify failed");
-			rBlacks = verifyNode(n.right);
-		}
-
-		if (n.red) {
-			if (n.left.red)
-				throw new RuntimeException("RB tree red-red child verify failed");
-			if (n.right.red)
-				throw new RuntimeException("RB tree red-red child verify failed");
-		}
-
-		if (lBlacks != rBlacks)
-			throw new RuntimeException("RB depth equality verify failed");
-		return lBlacks + (n.red ? 0 : 1);
-	}
-
-	// Search the tree and return true if this node is found
-	final EventNode find(long schedTick, int priority) {
-		EventNode curr = root;
-		while (true) {
-			if (curr == EventNode.nilNode) return null;
-			int comp = curr.compare(schedTick, priority);
-			if (comp == 0) {
-				return curr;
-			}
-			if (comp < 0) {
-				curr = curr.right;
-				continue;
-			}
-			curr = curr.left;
-			continue;
-		}
-	}
-
-	final int verifyNodeCount() {
-		if (root == EventNode.nilNode) return 0;
-		return countNodes(root);
-	}
-
-	/**
-	 * 递归的统计节点n的子节点数，（包括自己）
-	 * @param n
-	 * @return
-	 */
-	private int countNodes(EventNode n) {
-		int count = 1;
-		if (n.left != EventNode.nilNode) {
-			count += countNodes(n.left);
-		}
-		if (n.right != EventNode.nilNode) {
-			count += countNodes(n.right);
-		}
-
-		return count;
-	}
 
 	/**
 	 * 可复用空闲节点链表，是一个只用到节点左子树的链表结构
@@ -534,6 +514,86 @@ class EventTree {
 	 */
 	private void clearFreeList() {
 		freeList = null;
+	}
+
+	// Verify the sorting structure and return the number of nodes
+	final int verify() {
+		if (root == EventNode.nilNode) {
+			return 0;
+		}
+
+		if (EventNode.nilNode.red == true)
+			throw new RuntimeException("nil node corrupted, turned red");
+		return verifyNode(root);
+	}
+
+
+
+	// ******************
+	// 测试验证红黑树相关代码
+	// ******************
+
+	private int verifyNode(EventNode n) {
+		int lBlacks = 0;
+		int rBlacks = 0;
+
+		if (n.left != EventNode.nilNode) {
+			if (n.compareToNode(n.left) != 1)
+				throw new RuntimeException("RB tree order verify failed");
+			lBlacks = verifyNode(n.left);
+		}
+		if (n.right != EventNode.nilNode) {
+			if (n.compareToNode(n.right) != -1)
+				throw new RuntimeException("RB tree order verify failed");
+			rBlacks = verifyNode(n.right);
+		}
+
+		if (n.red) {
+			if (n.left.red)
+				throw new RuntimeException("RB tree red-red child verify failed");
+			if (n.right.red)
+				throw new RuntimeException("RB tree red-red child verify failed");
+		}
+
+		if (lBlacks != rBlacks)
+			throw new RuntimeException("RB depth equality verify failed");
+		return lBlacks + (n.red ? 0 : 1);
+	}
+
+	// Search the tree and return true if this node is found
+	final EventNode find(long schedTick, int priority) {
+		EventNode curr = root;
+		while (true) {
+			if (curr == EventNode.nilNode) return null;
+			int comp = curr.compare(schedTick, priority);
+			if (comp == 0) {
+				return curr;
+			}
+			if (comp < 0) {
+				curr = curr.right;
+				continue;
+			}
+			curr = curr.left;
+			continue;
+		}
+	}
+
+	final int verifyNodeCount() {
+		if (root == EventNode.nilNode) return 0;
+		return countNodes(root);
+	}
+
+
+	private int countNodes(EventNode n) {
+		int count = 1;
+		if (n.left != EventNode.nilNode) {
+			count += countNodes(n.left);
+		}
+		if (n.right != EventNode.nilNode) {
+			count += countNodes(n.right);
+		}
+
+		return count;
 	}
 
 }
